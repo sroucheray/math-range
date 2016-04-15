f79af45610151327c437602781ca78bbe27d4ba5
sroucheray <stephane.roucheray@gmail.com>
2016-04-15 12:23:59 +0200

    Export as default Object as well as individual functions

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
- export * from './src/scale.js';
- export * from './src/random.js';
+import { random, randomInt, randomIntInclusive } from './src/random.js';
+import { scale, curriedScale, curriedNormalize, normalize } from './src/scale.js';
+
+export * from './src/random.js';
+export * from './src/scale.js';
+
+export default {
+    random,
+    randomInt,
+    randomIntInclusive,
+    scale,
+    curriedScale,
+    curriedNormalize,
+    normalize
+};
